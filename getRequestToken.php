46074b8d32f5d61f4c0c97db1d60083c1349a8fd
AndreFace <andre.haggstrom.one@gmail.com>
2019-07-29 17:08:19 +0200

    Update getRequestToken.php

--- getRequestToken.php
+++ getRequestToken.php
@@ -2,7 +2,7 @@
 
 	require_once 'common.php';
 
-	$consumer = new HTTP_OAuth_Consumer(constant('PUBLIC_KEY'), constant('PRIVATE_KEY'), $_SESSION['token'], $_SESSION['tokenSecret']);
+	$consumer = new HTTP_OAuth_Consumer(constant('FEHUVEW84RAFR5SP22RABURUPHAFRUNU'), constant('ZUXEVEGA9USTAZEWRETHAQUBUR69U6EF'), $_SESSION['1feeb2dce6a5a1d1c8eca2349a4dcde505d25ef85'], $_SESSION['56f1b5d1582dfdb423b25cb7cf506a29']);
 	
 	try {
 	        $consumer->getAccessToken(constant('ACCESS_TOKEN'));	       
