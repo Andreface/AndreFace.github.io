2ae22e99aaba219e17aa45c497d2cf818535503b
AndreFace <andre.haggstrom.one@gmail.com>
2019-07-29 17:06:59 +0200

    Update getRequestToken.php

--- getRequestToken.php
+++ getRequestToken.php
@@ -1,16 +1,19 @@
+	<?php
 
+	require_once 'common.php';
 
-$consumer = new HTTP_OAuth_Consumer(constant('FEHUVEW84RAFR5SP22RABURUPHAFRUNU'), constant('ZUXEVEGA9USTAZEWRETHAQUBUR69U6EF'));
-$consumer->getRequestToken(constant(
-    'REQUEST_TOKEN'), constant('BASE_URL').'/getAccessToken.php'
-);
-When we have those we store them in the session.
-
-$_SESSION['token'] = $consumer->getToken();
-$_SESSION['tokenSecret'] = $consumer->getTokenSecret();
-Redirect the user to the authorization page
-
-$url = $consumer->getAuthorizeUrl(
-    constant('AUTHORIZE_TOKEN')
-);
-header('Location:'.$url);
+	$consumer = new HTTP_OAuth_Consumer(constant('PUBLIC_KEY'), constant('PRIVATE_KEY'), $_SESSION['token'], $_SESSION['tokenSecret']);
+	
+	try {
+	        $consumer->getAccessToken(constant('ACCESS_TOKEN'));	       
+	        $_SESSION['accessToken'] = $consumer->getToken();
+	        $_SESSION['accessTokenSecret'] = $consumer->getTokenSecret();
+	
+	        header('Location:index.php');
+	} catch (Exception $e) {
+	        ?>
+	                <p>Authorization failed!</p>
+	                <p><a href="index.php">Go back</a></p>
+	        <?php
+	}
+	
